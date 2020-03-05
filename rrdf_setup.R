9025ea4c504d62ecda9a0a56e7e44a8315848816
Mark A. Miller <mamillerpa@gmail.com>
2020-03-05 14:57:07 -0500

    tidy... excessive?

--- rrdf_setup.R
+++
@@ -1,7 +0,0 @@
-
-install.packages("rJava") # if not present already
-library(rJava)
-install.packages("devtools") # if not present already
-library(devtools)
-install_github("egonw/rrdf", subdir="rrdflibs")
-install_github("egonw/rrdf", subdir="rrdf", build_vignettes = FALSE)
